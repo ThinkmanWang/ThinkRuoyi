a16df3c7b1401b9b9dd29897af0532c0eb278b58
Thinkman Wang <wangxf1985@gmail.com>
2020-04-28 14:50:23 +0800

    update

---
+++ ruoyi-ui/src/views/business/test/TestUpload.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="wrapper">
+      <el-upload
+        :action="uploadImgUrl"
+        :headers="headers"
+        list-type="picture-card"
+        :on-preview="handlePictureCardPreview"
+        :on-remove="handleRemove">
+        <i class="el-icon-plus"></i>
+      </el-upload>
+
+      <el-dialog :visible.sync="dialogVisible">
+        <img width="100%" :src="dialogImageUrl" alt="">
+      </el-dialog>
+    </div>
+</template>
+<style lang="scss" scoped>
+
+</style>
+
+<script>
+    import { getToken } from '@/utils/auth'
+
+    export default {
+        name: "TestUpload"
+        , data() {
+            return {
+                dialogImageUrl: ''
+                , dialogVisible: false
+                , uploadImgUrl: process.env.VUE_APP_BASE_API + "/common/upload" // 上传的图片服务器地址
+                , headers: {
+                    Authorization: 'Bearer ' + getToken()
+                }
+            }
+        }
+        , created() {
+        }
+        , mounted() {
+        }
+        , beforeDestroy() {
+        }
+        , watch: {}
+        , computed: {}
+        , methods: {
+            handleRemove(file, fileList) {
+                console.log(file, fileList);
+            }
+            , handlePictureCardPreview(file) {
+                this.dialogImageUrl = file.url;
+                this.dialogVisible = true;
+            }
+        }
+    }
+</script>
+
