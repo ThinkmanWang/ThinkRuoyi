f19baf00e4fba1125722c52e19571120da564b6d
Thinkman Wang <wangxf1985@gmail.com>
2021-09-22 17:09:02 +0800

    update

---
+++ ruoyi-ui/src/views/business/testInput/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="wrapper">
+
+    </div>
+</template>
+<style lang="scss" scoped>
+
+</style>
+
+<script>
+    export default {
+        name: "index"
+        , data() {
+            return {}
+        }
+        , created() {
+        }
+        , mounted() {
+        }
+        , beforeDestroy() {
+        }
+        , watch: {}
+        , computed: {}
+        , methods: {}
+    }
+</script>
+
