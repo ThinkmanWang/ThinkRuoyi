b109ac8f41396431f1b71e08c2d6887c50c2a232
Thinkman Wang <wangxf1985@gmail.com>
2022-02-22 14:15:55 +0800

    update

---
+++ ruoyi-ui/src/views/test/link/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <iframe id="test-iframe" ref="main-iframe" :src="http://baidu.com" ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
