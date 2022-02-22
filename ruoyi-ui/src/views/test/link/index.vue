1121cf7684d2c00dfc2d4091bd5d3342dc9b4bb5
Thinkman Wang <wangxf1985@gmail.com>
2022-02-22 14:39:31 +0800

    update

--- ruoyi-ui/src/views/test/link/index.vue
+++ ruoyi-ui/src/views/test/link/index.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    <iframe id="test-iframe" ref="main-iframe" :src="http://baidu.com" ></iframe>
+    <iframe id="test-iframe" ref="main-iframe" :src="src" ></iframe>
   </div>
 </template>
 
 <script>
 export default {
   name: "index"
+
+  , computed: {
+    src() {
+      return "https://baidu.com"
+    }
+  }
 }
 </script>
 
